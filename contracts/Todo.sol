25716b170661dce9c569193feb9b646289692474
D4n13l <abrahamdanielu793@gmail.com>
2024-02-13 17:18:47 +0100

    new

---
+++ contracts/Todo.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Todo {
+    struct Todos {
+        string title;
+        string description;
+        bool isdone;
+    }
+
+    Todos [] todolist;
+
+    function createTodo () public {
+        todolist.push();
+    }
+
+    function updateTitle (string memory _title, uint i) public {
+        todolist[i].title = _title;
+    }
+    function updateDescription (string memory _description, uint i) public {
+        todolist[i].description = _description;
+    }
+    function isDoneToggle (uint i) public{
+        if (todolist[i].isdone == false ){
+            todolist[i].isdone = true;
+        }
+        else if (todolist[i].isdone == true){
+            todolist[i].isdone == false;
+        }
+    }
+
+    function deleteTodo (uint id) public {
+        for(uint i=0; i<todolist.length; i++){
+            todolist[id] = todolist[id+i];
+        }
+        todolist.pop();
+    }
+
+    function getTodo (uint i) public view returns (string memory, string memory, bool){
+        return (todolist[i].title, todolist[i].description, todolist[i].isdone);
+    }
+}
